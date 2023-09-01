4e86cdf896be90d1379f45bc75adb07a9ed78e7b
Chloe Law <bibiobraggins@gmail.com>
2023-09-01 10:38:38 -0600

    ws plug cleaning

--- lib/spigot/transports/ws/plug.ex
+++ lib/spigot/transports/ws/plug.ex
@@ -9,18 +9,21 @@ defmodule Spigot.Transports.WS.Plug do
 
   @impl true
   def call(%{request_path: "/", method: "GET"} = conn, options) do
-    conn = Plug.Conn.put_resp_header(conn, "Sec-Websocket-Protocol", "sip")
-
-    WebSockAdapter.upgrade(
-      conn,
-      Spigot.Transports.WS.Server,
-      Keyword.put(options, :peer, Plug.Conn.get_peer_data(conn)),
-      timeout: 60_000,
-      validate_utf8: true
-    )
+    if Plug.Conn.get_req_header(conn, "Sec-Websocket-Protocol") == ["sip"] do
+      conn = Plug.Conn.put_resp_header(conn, "Sec-Websocket-Protocol", "sip")
+      WebSockAdapter.upgrade(
+        conn,
+        Spigot.Transports.WS.Server,
+        Keyword.put(options, :peer, Plug.Conn.get_peer_data(conn)),
+        timeout: 60_000,
+        validate_utf8: true
+      )
+    else
+      Plug.Conn.send_resp(conn, 404, "Not Found")
+    end
   end
 
   def call(conn, _options) do
-    Plug.Conn.send_resp(conn, 404, "404: must be a sip websocket")
+    Plug.Conn.send_resp(conn, 501, "Not Implemented")
   end
 end
